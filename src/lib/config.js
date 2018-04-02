66d5f0af3ceaaaea3318063f4589a2a24078e63c
ropynn <ropynn@qq.com>
2018-04-02 10:36:09 +0800

    不忽略dist文件夹

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,7 +1,7 @@
 export default {
   install(Vue, options) {
-    Vue.prototype.api = 'http://tsd.yzidea.com';
-    // Vue.prototype.api = 'http://shop.lyhctech.com';
+    // Vue.prototype.api = 'http://tsd.yzidea.com';
+    Vue.prototype.api = 'http://shop.lyhctech.com';
     // Vue.prototype.api = 'http://tsa.yzidea.com';
     // Vue.prototype.api = 'http://shop.doremes.com';
   }
